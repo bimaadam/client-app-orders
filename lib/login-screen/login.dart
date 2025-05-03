809c8bdf6601f5a41fc0abfa43e4bd863c9b4eef
bimaadam <rinbim.dev@gmail.com>
2025-05-03 11:32:13 +0700

    fix bg colour

--- lib/login-screen/login.dart
+++ lib/login-screen/login.dart
@@ -6,7 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color.fromARGB(255, 255, 240, 240),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
